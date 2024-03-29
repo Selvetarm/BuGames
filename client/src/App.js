4355e2bfa3d3fbfef7fc57ed5356ea98a7c64dcd
Alex Volkov <aspidadm@gmail.com>
2024-03-29 14:06:05 +0300

    Hello

---
+++ client/src/App.js
@@ -0,0 +1,89 @@
+import React, { useCallback, useRef, useState } from 'react';
+import ReactFlow, {
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from 'reactflow';
+
+import { initialNodes, initialEdges } from './nodes-edges';
+import ContextMenu from './ContextMenu';
+
+import InfoNode from './InfoNode';
+
+import 'reactflow/dist/style.css';
+import './style.css';
+
+const nodeTypes = {
+  infoNode: InfoNode,
+};
+
+const Flow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [menu, setMenu] = useState(null);
+  const ref = useRef(null);
+  const yPos = useRef(0);
+
+  const onConnect = useCallback(
+    (params) => setEdges((els) => addEdge(params, els)),
+    [setEdges],
+  );
+  
+  const onNodeDoubleClick = useCallback((event, node) => {
+    yPos.current += 50;
+    setNodes((nds) => nds.concat({
+      id: `${Math.random()}`,
+      type: 'infoNode',
+      position: { x: 100, y: yPos.current },
+      data: {testProp: node.data.testProp, testProp2: node.data.testProp2},
+    }));
+    console.log('dblClick', node);
+  }, [setNodes]);
+
+  const onNodeContextMenu = useCallback(
+    (event, node) => {
+      // Prevent native context menu from showing
+      event.preventDefault();
+
+      // Calculate position of the context menu. We want to make sure it
+      // doesn't get positioned off-screen.
+      const pane = ref.current.getBoundingClientRect();
+      setMenu({
+        id: node.id,
+        testProp: node.testProp,
+        testProp2: node.testProp2,
+        top: event.clientY < pane.height - 200 && event.clientY,
+        left: event.clientX < pane.width - 200 && event.clientX,
+        right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
+        bottom:
+          event.clientY >= pane.height - 200 && pane.height - event.clientY,
+      });
+    },
+    [setMenu],
+  );
+
+  // Close the context menu if it's open whenever the window is clicked.
+  const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
+
+  return (
+    <ReactFlow
+      ref={ref}
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onPaneClick={onPaneClick}
+      onNodeContextMenu={onNodeContextMenu}
+      onNodeDoubleClick={onNodeDoubleClick}
+      nodeTypes={nodeTypes}
+      fitView
+    >
+      <Background />
+      {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
+    </ReactFlow>
+  );
+};
+
+export default Flow;
---
+++ client/src/InfoNode.js
@@ -0,0 +1,89 @@
+import React, { memo } from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons'
+import {
+  Handle,
+  Position,
+} from 'reactflow';
+
+const Panel = styled.div`
+  width: 300px;
+  text-align: left;
+`
+
+const PanelHeading = styled.div`
+  background-color: #000000;
+  color: #ffffff;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+`
+
+const ToggleButtonWrapper = styled.div`
+  transform: ${props => props.isCollapsed ? 'rotate(0deg)' : 'rotate(180deg)'}
+`
+
+const PanelContent = styled.div`
+  border: 1px solid #000000;
+  border-top: none;
+  padding: 0 20px;
+  color: #000000;
+  overflow: hidden;
+  height: ${props => props.isCollapsed ? 0 : 'auto'};
+`
+
+const PanelContentInner = styled.div`
+  padding: 20px 0;
+`
+
+export default memo((node) => {
+  const [isCollapsed, setIsCollapsed] = React.useState(true)
+
+  const togglePanel = () => {
+    setIsCollapsed(prevState => !prevState)
+  }
+
+  console.log(node.data.elements);
+
+  return (
+    <>
+    <div>{node.data.label}</div>
+
+    <Panel>
+      <PanelHeading onClick={togglePanel}>
+        <span>Дополнительно</span>
+        <ToggleButtonWrapper isCollapsed={isCollapsed}>
+          <FontAwesomeIcon icon={faChevronCircleDown} />
+        </ToggleButtonWrapper>
+      </PanelHeading>
+      <PanelContent isCollapsed={isCollapsed}>
+        <PanelContentInner>
+        {Object.entries(node.data.elements).map(([key, subject], i) => (
+          <li className="travelcompany-input" key={i}>
+              <span className="input-label">key: {i} Name: {subject.name}</span>
+          </li>
+        ))}
+        </PanelContentInner>
+      </PanelContent>
+    </Panel>
+
+    <Handle
+      type="target"
+      position={Position.Left}
+      style={{ background: '#555' }}
+      onConnect={(params) => console.log('handle onConnect', params)}
+
+    />
+    <Handle
+      type="source"
+      position={Position.Right}
+      id="a"
+      style={{background: '#555'}}
+
+    />
+  </>
+  );
+});
